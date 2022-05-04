51cb125fbd168c7440b2c4cf5eb60eaa95eda49a
Jeison Echeverria <jeisonecheverria22@gmail.com>
2022-05-04 16:27:06 -0500

    Se inicia el modulo de JPA que posteriormente integraremos a nuestro portal de EmpleosAPP

---
+++ src/test/java/com/jecheverria/JpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jecheverria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
